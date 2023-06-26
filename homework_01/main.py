f7de5e3842221743a6678d761e6322aa3eb3cc7a
krakatand <137721036+krakatand@users.noreply.github.com>
2023-06-26 09:00:36 +0300

    Add corrected func is_prime in homework_01

--- homework_01/main.py
+++ homework_01/main.py
@@ -30,7 +30,7 @@ def is_prime(number):
         return True
     else:
         count = 0
-        for i in range(number + 1):
+        for i in range(1,number + 1):
             if number % i == 0:
                 count += 1
             else:
